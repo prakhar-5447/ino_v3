c769b3a4670e4cdb8f7287fbfa2689980de6eba7
pratham-0094 <sahupratham022003@gmail.com>
2022-10-02 22:31:36 +0530

    signup page done

---
+++ src/app/component/auth/signup/signup.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent implements OnInit {
+  login!: FormGroup;
+
+  constructor() {
+    this.login = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
+
+  ngOnInit(): void {}
+  create_account() {}
+}
