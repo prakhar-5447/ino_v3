dc19c622996bc615e2d976805f73e0b74fbaeb6b
pratham-0094 <sahupratham022003@gmail.com>
2022-10-03 12:37:57 +0530

    pages created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,14 @@ import { MatInputModule } from '@angular/material/input';
 
 import { LoginComponent } from './component/auth/login/login.component';
 import { SignupComponent } from './component/auth/signup/signup.component';
+import { UserComponent } from './component/user/user.component';
+import { SettingComponent } from './component/setting/setting.component';
+import { SearchComponent } from './component/search/search.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { FollowedComponent } from './component/followed/followed.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SignupComponent],
+  declarations: [AppComponent, LoginComponent, SignupComponent, UserComponent, SettingComponent, SearchComponent, SidebarComponent, FollowedComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/component/followed/followed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-followed',
+  templateUrl: './followed.component.html',
+  styleUrls: ['./followed.component.css']
+})
+export class FollowedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
