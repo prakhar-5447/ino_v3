0ae92885c365f010970ed8dff786f263740963ad
pratham-0094 <sahupratham022003@gmail.com>
2022-11-04 19:12:17 +0530

    add all component routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/auth/login/login.component';
 import { SignupComponent } from './component/auth/signup/signup.component';
+import { FollowedComponent } from './component/followed/followed.component';
+import { SearchComponent } from './component/search/search.component';
+import { SettingComponent } from './component/setting/setting.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { UserComponent } from './component/user/user.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: SidebarComponent,
+    children: [
+      { path: '', component: UserComponent },
+      { path: 'search', component: SearchComponent },
+      { path: 'followed', component: FollowedComponent },
+      { path: 'setting', component: SettingComponent },
+    ],
+  },
   {
     path: 'login',
     component: LoginComponent,
