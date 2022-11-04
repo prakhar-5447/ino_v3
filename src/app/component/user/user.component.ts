224ee1434a67933cf3df3599f21a24b582ff71e5
pratham-0094 <sahupratham022003@gmail.com>
2022-11-04 19:11:48 +0530

    design user info section

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
+  name!: String;
+  phone_no!: String;
+  email!: String;
+  description!: String;
+  socials = [1, 2, 3, 4, 5, 6];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.name = 'pratham sahu';
+    this.phone_no = '+91 8349378115';
+    this.email = 'sahupratham022003@gmail.com';
+    this.description =
+      'jdlkasjd sdjasldj askdjaskldj sjdkj sjdksjd asjdksj skjdakasjdsaad sakd sajdksajsa d d skajd k akjdk jk lk k j k jfkjfkf dkj kfjkdfj kjfkdjf akdfjaldja adjaksdj ad askdjakslj aada skdjaslk adjaksdjalkjd adaksjda sd adajdlajd a kjsdlajs asjldaj saskdjalksjd aaa sdkjaldja  askdjaljdas kdjalsdja d asdjlkasjdlkasa dasjdlkaj sd akdjald asjd kasjd asd aksjdlaksdas dalsjdlaksjdas dad jaslkd s adkasjdlkasjd ajdas jd alsjdalskd kdjlas ds dsl d lsj dlksajad s dksajadlkajds  sdjasldj asakdsa dsjaldkjsada sda sldjaslkdja sdas d asdkjalskjdaksl d asa dklasjdkljddjalksjd aa dlajdlkasjdkasjd lkasja sda ';
   }
 
+  ngOnInit(): void {}
 }
