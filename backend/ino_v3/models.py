45e8f11f6356c6754fce5ba519f1c717806dbad0
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-11-10 19:11:57 +0530

    all models create

--- backend/ino_v3/models.py
+++ backend/ino_v3/models.py
@@ -17,63 +17,82 @@ class Signup(models.Model):
     Id = models.AutoField(primary_key=True)
     Name = models.CharField(max_length=30)
     Username = models.CharField(max_length=30)
+    Avatar = models.CharField(max_length=50)
     Email = models.CharField(max_length=50)
     Password = models.CharField(max_length=16)
+    Description = models.CharField(max_length=100)
 
 
 class Login(models.Model):
     Id = models.AutoField(primary_key=True)
     Name = models.CharField(max_length=30)
     Username = models.CharField(max_length=30)
+    Avatar = models.CharField(max_length=50)
     Email = models.CharField(max_length=50)
+    Description = models.CharField(max_length=100)
 
 
-# class Tech(models.Model):
-#     name = models.CharField(max_length=20)
-
-#     class Meta:
-#         abstract = True
-
-
-# class TechForm(forms.ModelForm):
-#     class Meta:
-#         model = Tech
-#         fields = (
-#             'name',
-#         )
+class Social(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Username = models.ForeignKey(
+        Signup, on_delete=models.CASCADE)
+    Instagram = models.CharField(max_length=100)
+    Twitter = models.CharField(max_length=100)
+    Github = models.CharField(max_length=100)
+    LinkedIn = models.CharField(max_length=100)
+    Portfolio = models.CharField(max_length=100)
+    Other = models.CharField(max_length=100)
 
 
-# class Step(models.Model):
-#     title = models.CharField(max_length=100)
+class Follow(models.Model):
+    id = models.CharField(max_length=3)
 
-#     class Meta:
-#         abstract = True
-
-
-# class StepForm(forms.ModelForm):
-#     class Meta:
-#         model = Step
-#         fields = ('title',)
+    class Meta:
+        abstract = True
 
 
-# class AcceptChallenge(models.Model):
-#     Id = models.AutoField(primary_key=True)
-#     Name = models.CharField(max_length=30)
-#     Description = models.CharField(max_length=50)
-#     Difficulty_level = models.CharField(max_length=10)
-#     ProjectImage = models.CharField(max_length=100)
-#     CurrentTask = models.IntegerField(default=0)
-#     Challenge = models.ForeignKey(
-#         Challenge, on_delete=models.CASCADE)
-#     Username = models.ForeignKey(
-#         Signup, on_delete=models.CASCADE)
-#     AcceptedDate = models.DateTimeField(default=datetime.now, blank=True)
-#     Url = models.CharField(max_length=50)
-#     Technology = models.ArrayField(
-#         model_container=Tech,
-#         model_form_class=TechForm
-#     )
-#     Progress = models.ArrayField(
-#         model_container=Step,
-#         model_form_class=StepForm
-#     )
+class FollowForm(forms.ModelForm):
+    class Meta:
+        model = Follow
+        fields = (
+            'id',
+        )
+
+
+class Tech(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        abstract = True
+
+
+class TechForm(forms.ModelForm):
+    class Meta:
+        model = Tech
+        fields = (
+            'name',
+        )
+
+
+class Followed(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Username = models.ForeignKey(
+        Signup, on_delete=models.CASCADE)
+    Followed = models.ArrayField(
+        model_container=Follow,
+        model_form_class=FollowForm
+    )
+
+
+class Project(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Username = models.ForeignKey(
+        Signup, on_delete=models.CASCADE)
+    Title = models.CharField(max_length=20)
+    ProjectImage = models.CharField(max_length=50)
+    Url = models.CharField(max_length=50)
+    Technology = models.ArrayField(
+        model_container=Tech,
+        model_form_class=TechForm
+    )
+    Description = models.CharField(max_length=100)
--- backend/ino_v3/serializers.py
+++ backend/ino_v3/serializers.py
@@ -5,10 +5,11 @@ from ino_v3.models import Signup, Login
 class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Signup
-        fields = ("Id", "Name", "Username", "Email", "Password")
+        fields = ("Id", "Name", "Username", "Avatar",
+                  "Email", "Password", "Description")
 
 
 class LoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = Login
-        fields = ("Id", "Name", "Username", "Email")
+        fields = ("Id", "Name", "Username", "Avatar", "Email", "Description")
