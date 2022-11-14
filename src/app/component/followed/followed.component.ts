00d30328d13153ddef3e53f64361cf63142b7a1c
pratham-0094 <sahupratham022003@gmail.com>
2022-11-14 21:14:06 +0530

    design followed page

--- src/app/component/followed/followed.component.ts
+++ src/app/component/followed/followed.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-followed',
   templateUrl: './followed.component.html',
-  styleUrls: ['./followed.component.css']
+  styleUrls: ['./followed.component.css'],
 })
 export class FollowedComponent implements OnInit {
+  index = [1, 2, 3, , 5, 6, 7, 8, 9, 10];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
