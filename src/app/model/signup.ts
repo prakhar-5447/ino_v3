57f57926b402dc2081092c3cfe923cbdf0d02184
pratham-0094 <sahupratham022003@gmail.com>
2022-11-17 21:20:18 +0530

    auth model created

---
+++ src/app/model/signup.ts
@@ -0,0 +1,10 @@
+export class signup {
+    Name!: String;
+    Username!: String;
+    Email!: String;
+    Avatar!: String;
+    Password!: String;
+    Description!: String;
+    Phone_no!:String
+  }
+  
