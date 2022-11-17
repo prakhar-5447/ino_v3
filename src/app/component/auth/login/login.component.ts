dcbd91dc76f6a5f7023b06af92c9a373dee7ebe0
pratham-0094 <sahupratham022003@gmail.com>
2022-11-17 21:21:23 +0530

    auth page completed

--- src/app/component/auth/login/login.component.ts
+++ src/app/component/auth/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { login } from 'src/app/model/login';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,22 +10,32 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  login!: FormGroup;
-  photo!: String;
-
-  constructor() {
-    this.login = new FormGroup({
-      name: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.required, Validators.email]),
+  loginForm!: FormGroup;
+  credential!: login;
+  constructor(private auth: AuthService, private router: Router) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.email]),
       password: new FormControl('', [
         Validators.required,
         Validators.minLength(8),
       ]),
-      phone_no: new FormControl('', [Validators.required]),
-      profile_photo: new FormControl(this.photo),
     });
   }
 
-  ngOnInit(): void {}
-  create_account() {}
+  ngOnInit(): void {
+    if (localStorage.getItem('authId')) {
+      this.router.navigateByUrl('/');
+    }
+  }
+
+  continue() {
+    if (this.loginForm.valid) {
+      this.credential = {
+        Email: this.loginForm.value['email'],
+        Password: this.loginForm.value['password'],
+      };
+      this.auth.login(this.credential);
+      this.loginForm.reset();
+    }
+  }
 }
--- src/app/component/auth/signup/signup.component.ts
+++ src/app/component/auth/signup/signup.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { signup } from 'src/app/model/signup';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,18 +10,65 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-  signup!: FormGroup;
-
-  constructor() {
-    this.signup = new FormGroup({
+  signupForm!: FormGroup;
+  photoImage!: String;
+  credential!: signup;
+  constructor(private auth: AuthService, private router: Router) {
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [
         Validators.required,
         Validators.minLength(8),
       ]),
+      phone_no: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(10),
+      ]),
     });
   }
 
-  ngOnInit(): void {}
-  create_account() {}
+  ngOnInit(): void {
+    if (localStorage.getItem('authId')) {
+      this.router.navigateByUrl('/');
+    }
+  }
+
+  @ViewChild('fileInput') fileInput: any;
+
+  create_account() {
+    const formData = new FormData();
+    const fi = this.fileInput.nativeElement;
+    const fileToUpload = fi.files[0];
+    formData.append('file', fileToUpload);
+    this.auth.uploadImage(formData).subscribe((Response: any) => {
+      this.photoImage = Response.msg;
+      if (this.signupForm.valid) {
+        this.credential = {
+          Name: this.signupForm.value['name'],
+          Email: this.signupForm.value['email'],
+          Username: this.signupForm.value['username'],
+          Password: this.signupForm.value['password'],
+          Description: 'My name is' + this.signupForm.value['name'],
+          Avatar: this.photoImage,
+          Phone_no: this.signupForm.value['phone_no'],
+        };
+        this.auth.signup(this.credential);
+        this.signupForm.reset();
+      }
+    });
+  }
+
+  numberOnly(event: any): Boolean {
+    const charcode = event.which ? event.which : event.keyCode;
+    if (charcode > 31 && (charcode < 48 || charcode > 57)) {
+      return false;
+    }
+    return true;
+  }
 }
