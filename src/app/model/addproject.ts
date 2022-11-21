daa1fb55077199051fe96387840e15eda207346c
pratham-0094 <sahupratham022003@gmail.com>
2022-11-21 20:03:27 +0530

    project model created

---
+++ src/app/model/addproject.ts
@@ -0,0 +1,8 @@
+export class addproject {
+  Username!: String;
+  Title!: String;
+  ProjectImage!: String;
+  Url!: String;
+  Technology!: any;
+  Description!: String;
+}
---
+++ src/app/model/project.ts
@@ -0,0 +1,9 @@
+export class project {
+  Id!: String;
+  Username!: String;
+  Title!: String;
+  ProjectImage!: String;
+  Url!: String;
+  Technology!: any;
+  Description!: String;
+}
