f0564b022fc90cbbc1e7086e77f411dee6234e25
pratham-0094 <sahupratham022003@gmail.com>
2022-12-01 13:01:15 +0530

    follow page modified

--- src/app/component/followed/followed.component.ts
+++ src/app/component/followed/followed.component.ts
@@ -2,6 +2,7 @@ import { Dialog } from '@angular/cdk/dialog';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { FollowService } from 'src/app/services/follow.service';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -11,22 +12,37 @@ import { ModalService } from 'src/app/services/modal.service';
 })
 export class FollowedComponent implements OnInit {
   followArray: any;
-  follow: any = [];
+  followed: any = [];
 
   constructor(
     private router: Router,
     private modal: ModalService,
     private dialog: Dialog,
-    private auth: AuthService
+    private auth: AuthService,
+    private follow: FollowService
   ) {
-    this.followArray = this.modal.getFollowList();
-    for (let i = 0; i < this.followArray.length; i++) {
-      this.auth.getuserId(this.followArray[i].id).subscribe((Response: any) => {
-        if (Response.success) {
-          this.follow.push(Response.msg);
-        }
-      });
+    const data = this.auth.checkAuth();
+    if (!data.success) {
+      this.router.navigateByUrl('/login');
     }
+    this.follow.getfollow(data.userId).subscribe((Response: any) => {
+      if (Response.success) {
+        this.modal.setFollowList(Response.msg[0]['Followed']);
+        this.modal.setId(Response.msg[0]['Id']);
+        this.followArray = this.modal.getFollowList();
+        for (let i = 0; i < this.followArray.length; i++) {
+          this.auth
+            .getuserId(this.followArray[i].id)
+            .subscribe((Response: any) => {
+              if (Response.success) {
+                this.followed.push(Response.msg);
+              }
+            });
+        }
+      } else {
+        this.follow.follow(data.userId);
+      }
+    });
   }
 
   ngOnInit(): void {}
